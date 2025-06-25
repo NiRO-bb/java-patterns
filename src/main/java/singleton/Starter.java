5668f5244900ea05aa0eea79c4034fc7530f1ec9
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-06-25 21:31:09 +0300

    docs: add Javadoc comments

--- src/main/java/singleton/Starter.java
+++ src/main/java/singleton/Starter.java
@@ -1,9 +1,24 @@
 package singleton;
 
+/**
+ * Demonstrates SingletonClass class work.
+ * <p>
+ * Contains {@code main} method that creates and uses insatnces of SingletonClass.
+ *
+ * @see SingletonClass
+ */
 public final class Starter {
 
     private Starter() {}
 
+    /**
+     * Program entry point.
+     * <p>
+     * Creates {@code singleton1} instance of SingletonClass, then tries to create new instance.
+     * Outputs the results.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
 
         SingletonClass singleton1 = SingletonClass.getInstance();
