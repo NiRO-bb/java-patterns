2ecc6a25de9c0e5ad1de3641d4eb27320817847f
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-06-26 16:54:50 +0300

    feat: add ConcreteDecoratorTwo class

---
+++ src/main/java/ConcreteDecoratorTwo.java
@@ -0,0 +1,22 @@
+public class ConcreteDecoratorTwo extends Decorator {
+
+    private Component component;
+
+    public ConcreteDecoratorTwo(Component component) {
+        super(component);
+        this.component = component;
+    }
+
+    @Override
+    public String getStatus() {
+        return String.format("(%s)", component.getStatus());
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("-----");
+        component.printStatus();
+        System.out.println("\n-----");
+    }
+
+}
