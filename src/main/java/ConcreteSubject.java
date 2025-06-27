7889a59c489635e9ad1757d6afede6f2534747b7
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-06-27 10:49:24 +0300

    feat: add ConcreteSubject class

---
+++ src/main/java/ConcreteSubject.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConcreteSubject implements Subject {
+
+    private List<Observer> observers = new LinkedList<>();
+
+    public void event() {
+        System.out.println("Something happened!");
+        notifyObservers();
+    }
+
+    @Override
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
+
+}
